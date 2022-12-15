6164e92ec56aefb1168b230c754a56bfff484dd6
Carter Pierre <carterspierre@gmail.com>
2022-12-15 16:47:22 -0500

    updating port

--- server.js
+++ server.js
@@ -65,5 +65,5 @@ app.use("/comment", commentRoutes)
 
 //Server Running
 app.listen(process.env.PORT, () => {
-  console.log("Server is running, you better catch it!");
+  console.log("Server is running, you better catch it!",process.env.PORT);
 });
